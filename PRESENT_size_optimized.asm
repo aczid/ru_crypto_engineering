a84d7b3a93dc162d7f00fe2b6acb4e215e1d23b7
Aram Verstegen <aram.verstegen@gmail.com>
2013-02-24 20:23:01 +0100

    Ditched one more byte and updated comments

--- PRESENT_size_optimized.asm
+++ PRESENT_size_optimized.asm
@@ -42,7 +42,7 @@
 SBOX:.db 0xc,0x5,0x6,0xB,0x9,0x0,0xA,0xD,0x3,0xE,0xF,0x8,0x4,0x7,0x1,0x2
 
 addRoundKey:
-	; state ^= roundkey (top 8 bytes of key register)
+	; state ^= roundkey (top 4 bytes of key register)
 	eor STATE0, KEY0
 	eor STATE1, KEY1
 	eor STATE2, KEY2
@@ -53,7 +53,7 @@ addRoundKey:
 ; approach stolen from KULeuven implementation
 
 ; splices 1 input byte over 4 output bytes, which will then each hold 2 bits
-; following a 4-bit period in the byte
+; following a 4-bit period in the input
 
 ; after 4 calls from different input registers we will have collected 4
 ; completed output bytes following this 4-bit period
@@ -84,14 +84,13 @@ redo_pLayerByte:
 ; procedure as its own procedure when key scheduling
 ; read from and writes to ITEMP
 sBoxByte:
-	;ror ITEMP
 	; input (low nibble)
 	mov ZL, ITEMP   ; load input
 	cbr ZL, 0xf0    ; clear high nibble in input
 
 	; output (low nibble)
 	lpm SBOX_OUTPUT, Z    ; load s-box output into temp register
-	cbr ITEMP, 0xf  ; clear low nibble in output register
+	cbr ITEMP, 0xf        ; clear low nibble in output register
 	or ITEMP, SBOX_OUTPUT ; save low nibble to output register
 
 	; fall through
@@ -104,12 +103,12 @@ sBoxHighNibble:
 	; output (high nibble)
 	lpm SBOX_OUTPUT, Z    ; load s-box output into temp register
 	swap SBOX_OUTPUT      ; move low nibble of s-box output to high nibble
-	cbr ITEMP, 0xf0 ; clear high nibble in output
+	cbr ITEMP, 0xf0       ; clear high nibble in output
 	or ITEMP, SBOX_OUTPUT ; save high nibble to output
 
 	ret
 
-; load 4 consequtive input bytes into state
+; load 4 consecutive input bytes into state
 load_input:
 	ld STATE0, X+
 	ld STATE1, X+
@@ -137,7 +136,7 @@ continue_rotate_left_i:
 	brne continue_rotate_left_i
 	ret
 
-; saves state bytes back to front
+; saves state bytes from back to front
 ; leaves 1 byte untouched in between each saved byte
 interleaved_output:
 	dec XL
@@ -150,26 +149,21 @@ interleaved_output:
 	st -X, OUTPUT3
 	ret
 
-; saves 4 consecutive bytes to RAM
-consecutive_output:
-	st X+, STATE0
-	st X+, STATE1
-	st X+, STATE2
-	st X+, STATE3
-	ret
-
 ; the main function called by the wrapper provided by the instructors
-; uses the T flag to transfer control to even and odd output procedures which do the final positioning of the pLayer output
+; uses the T flag to transfer control to even and odd output procedures which
+; do the final interleaved placement of the pLayer output in SRAM
 encrypt:
 	init:
+		; initialize round counter
 		ldi ROUND_COUNTER, 1
 		; initialize s-box
 		ldi ZH, high(SBOX<<1)
-		; load first 4 bytes (high/left part)
+		; load first 4 input bytes (high/left part)
 		rcall load_input
+		; point at the end of the key bytes
 		adiw XL, 14
+		; load key from SRAM, back to front
 		load_key:
-			; loads key from SRAM, back to front
 			ld KEY9, -X
 			ld KEY8, -X
 			ld KEY7, -X
@@ -180,6 +174,7 @@ encrypt:
 			ld KEY2, -X
 			ld KEY1, -X
 			ld KEY0, -X
+		; point at the second 4 input bytes (low/right part)
 		subi XL, 4
 		; start round
 		rjmp encrypt_update
@@ -189,12 +184,13 @@ encrypt:
 		rcall load_input
 		; copy odd bytes into position
 		rcall interleaved_output
-		; rotate key register by 4 bytes to align with the low/right part
+		; rotate key register by 4 bytes to align its first 4 bytes with the next 4 bytes of input
 		ldi ITEMP, 32
 		rcall rotate_left_i
-		set ; set T flag
-		; do next 4 bytes (fall through)
-
+		set ; set T flag to transfer control to even_output after this round
+		; do next 4 bytes
+		; fall through
+	; main round procedure
 	encrypt_update:
 		; apply round key
 		rcall addRoundKey
@@ -218,8 +214,8 @@ encrypt:
 			rcall sBoxByte
 			mov STATE3, ITEMP
 
-		; permutes bit positions following a 4-bit period
-		; stolen from KULeuven implementation and optimized
+		; permutes bit positions in the state following a 4-bit period
+		; stolen from KULeuven implementation and optimized for size
 		pLayer:
 			; map output bytes
 			mov ITEMP,STATE3
@@ -231,21 +227,28 @@ encrypt:
 			mov ITEMP,STATE0
 			rcall pLayerByte
 
-		; check if we are doing the high/left block, if so output odd state bytes and prepare for the low/right block
+		; check the T bit, if it's not set we are working on the
+		; high/left block
 		brtc odd_output
 
-		; this is the end of the round
-		; copy even state bytes into position
+		; otherwise, we are working on the low/right block
 		even_output:
+			; copy even state bytes into position
 			adiw XL, 9
 			rcall interleaved_output
 			dec XL
-			; load next 4 left/high bytes
+
+			; load next 4 left/high input bytes
 			rcall load_input
+
 			; rotate back key register
 			ldi ITEMP, 48
 			rcall rotate_left_i
-			clt ; clear T flag
+
+			; clear T flag to send the next round through the
+			; odd_output procedure first
+			clt
+		; fall through
 
 		; schedule key for next round
 		schedule_key:
@@ -258,25 +261,28 @@ encrypt:
 			ldi ITEMP, 55
 			rcall rotate_left_i
 
-			; 2: sbox high nibble of key
+			; 2: s-box high nibble of key
 			mov ITEMP, KEY0
 			rcall sBoxHighNibble
 			mov KEY0, ITEMP
 
+			; round++
 			inc ROUND_COUNTER	
 
 		; check round counter, break when it reaches 32 (meaning the key scheduled is k_32)
 		cpi ROUND_COUNTER, 32
-		brne encrypt_update ; continue
+		brne encrypt_update ; do next round
 		; fall through
 	final:
-		; apply final round key (left)
+		; apply final round key (high/left)
 		rcall final_part
 
-		; apply final round key (right)
+		; load low/right bytes
 		rcall load_input
+		; adjust key register
 		ldi ITEMP, 32
 		rcall rotate_left_i
+		; apply final round key (low/right)
 		rcall final_part
 
 	ret
@@ -285,5 +291,11 @@ encrypt:
 final_part:
 	rcall addRoundKey
 	subi XL, 4
-	rcall consecutive_output
+
+	; saves 4 consecutive bytes to RAM
+	st X+, STATE0
+	st X+, STATE1
+	st X+, STATE2
+	st X+, STATE3
 	ret
+
