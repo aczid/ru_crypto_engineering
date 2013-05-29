11bb5190226ed441841aab666994d221ce24a86b
Aram Verstegen <aram.verstegen@gmail.com>
2013-05-29 19:08:00 +0200

    Missed an instruction when counting

--- PRESENT_size_optimized.asm
+++ PRESENT_size_optimized.asm
@@ -30,7 +30,7 @@
 
 #ifdef DECRYPTION
 #define PACKED_SBOXES ; Use packed s-boxes (which need to be unpacked)
-                      ; This saves 4 bytes
+                      ; This saves 2 bytes
 #endif
 
 #ifdef PACKED_SBOXES
